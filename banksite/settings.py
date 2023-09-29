817a83076814a60994f5b0321ed57b357ebda719
Martin Ovington <MartinOvington@msn.com>
2023-09-29 13:33:08 +0100

    fixed minor issue with code not being correctly commented out

--- banksite/settings.py
+++ banksite/settings.py
@@ -88,9 +88,10 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
+AUTH_PASSWORD_VALIDATORS = []
+"""
 AUTH_PASSWORD_VALIDATORS = [
-    """
+
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
-"""
-]
 
+]
+"""
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
